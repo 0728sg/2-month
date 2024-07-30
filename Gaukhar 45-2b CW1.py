55840534e05aff6cc451e783438a706f03064829
Cheremuxa <gaukhar.2006@bk.ru>
2024-07-30 20:51:26 +0600

    HW

---
+++ Gaukhar 45-2b CW1.py
@@ -0,0 +1,23 @@
+#VENV, CMD, GIT
+#modules in programm language
+# p = 1, 'str', True, 1.2, [], {}, (), None
+# print(type(p))
+# p.count()
+#VENV - виртуальное окружение
+#print(colorama.Back.Black, colorama.Fone.Green)
+# print('Hello World')
+tprint('Hello World')
+#PIP:
+
+#pip install name: устанавливают зависимость
+#pip freeze: показывает
+download art
+pip freeze > reg.txt
+import art: from art import tprint
+1 раз
+git init :связывает ит с вашим проектом
+git remote add origin ссылка на ваш репозиторий: связывает ваш проект с репозиторием
+всегда
+git add . :добавляет файлы
+git commit -m 'название'
+git push origin master\main
